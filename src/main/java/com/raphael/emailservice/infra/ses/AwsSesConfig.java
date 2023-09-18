e6507e4208ec39e4791c3d9f0891f27a3985a0fa
Raphael de Mello - RDM <raph.mello@gmail.com>
2023-09-18 19:37:08 -0300

    Adds configurable properties to set email and region

--- src/main/java/com/raphael/emailservice/infra/ses/AwsSesConfig.java
+++ src/main/java/com/raphael/emailservice/infra/ses/AwsSesConfig.java
@@ -3,14 +3,20 @@ package com.raphael.emailservice.infra.ses;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsSesConfig {
+
+    @Value(value = "${aws.region}")
+    private String region;
+
     @Bean
     public AmazonSimpleEmailService amazonSimpleEmailService() {
+        final Regions regions = Regions.fromName(region);
         return AmazonSimpleEmailServiceClientBuilder.standard()
-                .withRegion(Regions.US_EAST_2).build();
+                .withRegion(regions).build();
     }
 }
--- src/main/java/com/raphael/emailservice/infra/ses/SesEmailService.java
+++ src/main/java/com/raphael/emailservice/infra/ses/SesEmailService.java
@@ -5,11 +5,15 @@ import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.model.*;
 import com.raphael.emailservice.adapters.EmailSenderGateway;
 import com.raphael.emailservice.core.exceptions.EmailServiceException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SesEmailService implements EmailSenderGateway {
     private final AmazonSimpleEmailService amazonSimpleEmailService;
+
+    @Value(value = "${aws.verified-email}")
+    private String verifiedEmail;
 
     public SesEmailService(AmazonSimpleEmailService amazonSimpleEmailService) {
         this.amazonSimpleEmailService = amazonSimpleEmailService;
@@ -18,7 +22,7 @@ public class SesEmailService implements EmailSenderGateway {
     @Override
     public void sendEmail(String toEmail, String subject, String body) {
         SendEmailRequest request = new SendEmailRequest()
-                .withSource("raph.mello@gmail.com")
+                .withSource(verifiedEmail)
                 .withDestination(new Destination().withToAddresses(toEmail))
                 .withMessage(
                         new Message()
